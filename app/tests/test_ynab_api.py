c63701a94d3453f0d32d8e9b6297a0cbb30cc805
jdcundiff <jd_cundiff@outlook.com>
2023-12-03 12:04:16 -0600

    Added unit testing folder in app/tests.

---
+++ app/tests/test_ynab_api.py
@@ -0,0 +1,17 @@
+# app/tests/test_ynab_api.py
+from django.test import TestCase
+import requests
+import os
+
+
+class YNABAPITest(TestCase):
+    def test_ynab_api_connection(self):
+        """ Test connection to the YNAB API """
+        api_key = os.getenv('YNAB_API_KEY')
+        headers = {'Authorization': f'Bearer {api_key}'}
+        response = requests.get('https://api.youneedabudget.com/v1/user', headers=headers)
+
+        # Check if the response status code is 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+        # Further checks can be added here, such as checking the structure of the response
