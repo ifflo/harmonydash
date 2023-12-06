a05903dbb0052d7f8744920729868c3f906ea284
jdcundiff <jd_cundiff@outlook.com>
2023-12-06 17:26:16 -0600

    - Registered `FinancialSettings` and `Transaction` models in `admin.py` for easy management via Django's admin interface.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
+# app/admin.py
 from django.contrib import admin
+from .models import UserProfile, Schedule, FinancialSettings, Transaction
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Schedule)
+admin.site.register(FinancialSettings)
+admin.site.register(Transaction)
