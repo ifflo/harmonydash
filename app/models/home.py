aee02b1d97eb8c3d66d7c330d7be9bacfc5e987d
jdcundiff <jd_cundiff@outlook.com>
2023-12-21 10:15:27 -0600

    Implement Singleton Pattern in Home Model
    This commit updates the Home model to enforce a singleton pattern, ensuring only one instance of the Home model can exist in the database. This change aligns with the application's design to manage financial information for a single household.
    
    - Added logic in the save method of Home model to prevent the creation of more than one instance.
    - Other changes have been reverted to maintain the current application structure.

--- app/models/home.py
+++ app/models/home.py
@@ -1,11 +1,18 @@
 # app/models/home.py
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class Home(models.Model):
     name = models.CharField(max_length=100)
     # Other fields as necessary (e.g., address, contact info)
+
+    def save(self, *args, **kwargs):
+        if Home.objects.exists() and not self.pk:
+            # If trying to create a new Home instance when one already exists
+            raise ValidationError('There can only be one Home instance.')
+        return super(Home, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
